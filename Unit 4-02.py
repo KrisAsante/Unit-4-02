42eb4b476144e4bc4316dcadeb1d0eb19cd2b222
Chris <36240719+KrisAsante@users.noreply.github.com>
2019-03-28 13:38:42 -0400

    Add files via upload

---
+++ Unit 4-02.py
@@ -0,0 +1,17 @@
+# Created by: Chris Asante
+# Created on: 11-Feb-2019
+# Created for: ICS3U
+# Daily Assignment – Unit 4-02
+# This is a program that rounds decimals
+
+def calculate_decimal(number, decimal_points):
+
+    answer = number * (10 ** decimal_points)
+    answer_2 = answer + 0.5
+    answer_3 = int(answer_2)
+    final_answer = answer_3 / (10 ** decimal_points)
+    print (final_answer)
+
+user_number = float(input("Enter the decimal number: "))
+user_decimal_points = int(input("Enter how may decimal points you want to round off to: "))
+calculate_decimal(user_number, user_decimal_points)
